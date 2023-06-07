a01716aee3bff06bae6f882117180df744119db1
ankit <ankit@LAPTOP-G419LKOB>
2023-06-07 15:13:11 +0530

    New Code commit

--- JavaLab/src/com/java/lab/Lab6.java
+++ JavaLab/src/com/java/lab/Lab6.java
@@ -7,11 +7,31 @@
  */
 package com.java.lab;
 
+import java.util.Scanner;
+
 public class Lab6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter the String name: ");
+		String str = sc.nextLine();
+
+		int count = 1;
+		int count1 = 1;
+
+		for (int i = 0; i < str.length() - 1; i++) {
+			if ((str.charAt(i) == ' ') && (str.charAt(i + 1) != ' ')) {
+				count++;
+
+			}
+		}
+		System.out.println("Number of words in a string: " + count);
+
+		for (int j = 0; j < str.length(); j++) {
+			count1 = str.length();
+		}
+		System.out.println("The String of letters number : " + count1);
 	}
-
 }
