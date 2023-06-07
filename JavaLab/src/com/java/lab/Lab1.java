e1b37b14f137028caea4f68c363193994977fd47
ankit <ankit@LAPTOP-G419LKOB>
2023-06-07 14:17:22 +0530

    Update space

--- JavaLab/src/com/java/lab/Lab1.java
+++ JavaLab/src/com/java/lab/Lab1.java
@@ -7,13 +7,9 @@ public class Lab1 {
 	public static void main(String[] args) {
 
 		System.out.println("Enter a reverse string name : \n");
-
 		Scanner sc = new Scanner(System.in);
-
 		String s = sc.nextLine();
-
 		char[] c = s.toCharArray();
-
 		for (int i = c.length; i >= 1; i--) {
 
 			System.out.print(c[i - 1]);
